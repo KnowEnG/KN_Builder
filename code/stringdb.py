44c45bc95edebc636a9d623f6c109fd20c0b766a
Corey Post <cspost3@gmail.com>
2015-07-07 21:01:40 +0000

    executable class.py and pylinted

--- code/stringdb.py
+++ code/stringdb.py
@@ -1,43 +1,47 @@
-from source_utils import *
-from urllib.request import urlopen
-from time import strptime, mktime
+"""Executable containing all of the functions needed to check if stringdb
+has updated. Checks the source and outputs the updated dictionary to file."""
+from utilities import SrcClass, compare_versions
+import urllib.request
 import re
+import time
 
 class Stringdb(SrcClass):
+    """Class providing the check functions for the source strindb"""
     def __init__(self):
+        """Constructor for the Stringdb class"""
         name = 'stringdb'
         url_base = 'http://string-db.org/'
-        aliases =   {"10090": "Mmus",
-                    "3702": "Atha",
-                    "4932": "Scer",
-                    "6239": "Cele",
-                    "7227": "Dmel",
-                    "9606": "Hsap"}
+        aliases = {"10090": "Mmus",
+                   "3702": "Atha",
+                   "4932": "Scer",
+                   "6239": "Cele",
+                   "7227": "Dmel",
+                   "9606": "Hsap"}
         super(Stringdb, self).__init__(name, url_base, aliases)
-    
+
     def get_source_version(self, alias):
-        """Returns the current version of source as a string. 
+        """Returns the current version of source as a string.
             Overwrites super metod to scrape with a source specific regex.
             Alias is ignored in this context."""
         if alias not in self.version:
-            response = urlopen(self.url_base)
+            response = urllib.request.urlopen(self.url_base)
             the_page = response.readlines()
             for line in the_page:
-                l = line.decode()
-                match = re.search('This is version ([^ ]*) of STRING', l)
+                d_line = line.decode()
+                match = re.search('This is version ([^ ]*) of STRING', d_line)
                 if match is not None:
                     response.close()
-                    for a in self.aliases:
-                        self.version[a] = match.group(1)
+                    for alias_name in self.aliases:
+                        self.version[alias_name] = match.group(1)
         return self.version[alias]
 
-    def get_source_version_date(self):
-        pass
-    
+    def get_source_version_date(self, alias):
+        return super(Stringdb, self).get_source_version(alias)
+
     def get_local_file_info(self, alias):
         """Returns the local file version information as defined in SrcClass"""
         return super(Stringdb, self).get_local_file_info(alias)
-    
+
     def get_remote_file_size(self, alias):
         """Returns the file size of source:alias as a string.
             Updates the url and calls the super method."""
@@ -50,9 +54,8 @@ class Stringdb(SrcClass):
         url = self.get_remote_url(alias)
         t_str = super(Stringdb, self).get_remote_file_modified(url)
         t_format = "%a, %d %b %Y %H:%M:%S %Z"
-        return mktime(strptime(t_str, t_format))
-        
-        
+        return time.mktime(time.strptime(t_str, t_format))
+
     def get_remote_url(self, alias):
         """Returns the remote url to be used for source:alias in a fetch"""
         version = self.get_source_version(alias)
@@ -63,4 +66,4 @@ class Stringdb(SrcClass):
 
 
 if __name__ == "__main__":
-    process_src(Stringdb())
+    compare_versions(Stringdb())
--- code/utilities.py
+++ code/utilities.py
@@ -1,64 +1,68 @@
-from urllib.request import urlopen
-from os import path
-from time import strptime, mktime
+"""Provides all of the utility files needed to check if a source has updated.
+Also contains the parent source class: SrcClass"""
+import urllib.request
+import os
+import json
+
+d_dir = '../raw_downloads/'
 
 class SrcClass(object):
-    'super class for sources'
+    """super class for sources"""
     def __init__(self, src_name, base_url, aliases):
         self.name = src_name
         self.url_base = base_url
         self.version = dict()
         self.aliases = aliases
-        
-    def get_source_version(self):
-        """Returns the source version information. 
+
+    def get_source_version(self, alias):
+        """Returns the source version information.
             By default, returns the empty string."""
         return ''
 
-    def get_source_version_date(self):
-        pass
-    
+    def get_source_version_date(self, alias):
+        """Returns the release date of the source:alias version"""
+        return ''
+
     def get_local_file_info(self, alias):
-        """Returns a dictionary contianing the local file information for 
-        source:alias which includes the local file name and a boolean that is 
-        True if the file exists. If the file does exist, the dictionary also 
+        """Returns a dictionary contianing the local file information for
+        source:alias which includes the local file name and a boolean that is
+        True if the file exists. If the file does exist, the dictionary also
         contains the local file size and modified date"""
-        d_dir = '../../raw_downloads/'
-        file = d_dir + self.name + '.' + alias + '.txt'
+        f_dir = d_dir + self.name + '/'
+        file = f_dir + self.name + '.' + alias + '.txt'
         local_dict = dict()
         local_dict['local_file_name'] = file
-        local_dict['local_file_exists'] = \
-            check_local_file_exists(file)
+        local_dict['local_file_exists'] = os.path.isfile(file)
         if not local_dict['local_file_exists']:
             return local_dict
-        local_dict['local_size'] = path.getsize(file)
-        local_dict['local_date'] = path.getmtime(file)
+        local_dict['local_size'] = os.path.getsize(file)
+        local_dict['local_date'] = os.path.getmtime(file)
         return local_dict
 
     def get_remote_file_size(self, remote_url):
-        """Returns the remote file size as described in the headers of 
+        """Returns the remote file size as described in the headers of
         remote_url"""
-        response = urlopen(remote_url)
+        response = urllib.request.urlopen(remote_url)
         return response.headers['content-length']
 
     def get_remote_file_modified(self, remote_url):
         """Returns the remote file modified date as described in the headers of
         remote url"""
-        response = urlopen(remote_url)
+        response = urllib.request.urlopen(remote_url)
         return response.headers['last-modified']
-    
-    def get_remote_url(self):
-        """Returns the url for fetching a file. By default this is the base 
+
+    def get_remote_url(self, alias):
+        """Returns the url for fetching a file. By default this is the base
         url"""
         return self.url_base
-        
+
 
 def compare_versions(srcObj):
     """
     compare_versions(srcObj) -> dict
-    
-    Returns dictionary of results of checking operations for each alias of 
-    the source including source, alias, alias_info, remote_url, remote_date, 
+
+    Returns dictionary of results of checking operations for each alias of
+    the source including source, alias, alias_info, remote_url, remote_date,
     remote_version, remote_size, local_file_name, local_file_exists, and
     fetch_needed.
     """
@@ -78,30 +82,25 @@ def compare_versions(srcObj):
             local_dict[alias]['local_file_name']
         version_dict[alias]['local_file_exists'] = \
             local_dict[alias]['local_file_exists']
-        
+
         if not local_dict[alias]['local_file_exists']:
             version_dict[alias]['fetch_needed'] = True
             continue
-        
+
         l_size = int(local_dict[alias]['local_size'])
         r_size = int(version_dict[alias]['remote_size'])
         l_date = float(local_dict[alias]['local_date'])
         r_date = float(version_dict[alias]['remote_date'])
-        
+
         if l_size != r_size:
             version_dict[alias]['fetch_needed'] = True
         elif l_date < r_date:
             version_dict[alias]['fetch_needed'] = True
         else:
             version_dict[alias]['fetch_needed'] = False
-    
-    return version_dict
 
-def check_local_file_exists(local_file_name):
-    """
-    check_local_file_exists(filename) -> bool
-    
-    Returns True if file exists at locations specified by local_file_name, 
-    False otherwise.
-    """
-    return path.isfile(local_file_name)
+    f_dir = d_dir + srcObj.name + '/'
+    with open(f_dir + srcObj.name + '_check.json', 'w') as outfile:
+        json.dump(version_dict, outfile, indent=4, sort_keys=True)
+    print(json.dumps(version_dict, indent=4, sort_keys=True))
+    return version_dict
