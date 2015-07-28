de32188f10e45d8862d9a9d8a1e7e7d72f50ba2c
cblatti3 <blatti@illinois.edu>
2015-07-28 17:45:58 -0500

    changes to curl command to chronos

--- code/run_check.sh
+++ code/run_check.sh
@@ -2,17 +2,16 @@
 
 # assume that master container is given a volume at "/shared/" that contains an
 # code/ folder with .py scripts and a raw_downloads/ folder for raw files.  
-mkdir -p /shared/code/
 mkdir -p /shared/raw_downloads/
 rm -r /shared/jobs/
 mkdir -p /shared/jobs/
 
 # print out command for new container for each source specific *.py
-BASE="\/workspace\/apps\/P1_source_check\/"   # change users to storage for cluster
+BASE="\/mnt\/storage\/blatti\/apps\/P1_source_check\/"   # change users to storage for cluster
 CMD="sh -c 'cd /code/; /usr/bin/time -v python3 SRC.py;'" 
 IMG="cblatti3\/python3:0.1"
 ctr=1
-CURL="curl -L -H 'Content-Type: application/json' -X POST -d /shared/jobs/NUM_SRC.json http://mmaster01.cse.illinois.edu:4400/scheduler/iso8601"
+CURL="curl -i -L -H 'Content-Type: application/json' -X POST -d@/shared/jobs/NUM_SRC.json mmaster02.cse.illinois.edu:4400/scheduler/iso8601"
 
 for i in `ls /shared/code/*.py | sed -e 's/.py//g' | sed -e 's/\/shared\/code\///g' | sort`; do  
 	echo "$i"
@@ -31,7 +30,7 @@ for i in `ls /shared/code/*.py | sed -e 's/.py//g' | sed -e 's/\/shared\/code\//
 	# set up and send the curl command
 	CURL_CMD=$(echo $CURL | sed -e "s/NUM/$ctr/g" | sed -e "s/SRC/$i/g")
 	echo $CURL_CMD
-#	$($CURL_CMD)
+	eval $CURL_CMD
 	
 	ctr=$(($ctr + 1))
 
