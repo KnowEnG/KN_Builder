249055c62cc64d444971878616fcbd48ea3ad08e
Corey Post <cspost3@gmail.com>
2015-09-05 15:01:47 +0000

    produces id_mapping tables

--- code/config_utilities.py
+++ code/config_utilities.py
@@ -7,11 +7,12 @@ Functions:
 Variables:
     DEFAULT_DOCKER_IMG: docker image to run pipeline steps
     DEFAULT_CURL_URL: address of chronos scheduler
+    DEFAULT_LOCAL_BASE: toplevel directory on local machine
     DEFAULT_CLOUD_BASE: toplevel directory on shared cloud storage
     
-    DEFAULT_LOCAL_BASE: toplevel directory on local machine
     DEFAULT_CODE_PATH: relative path of code dir from toplevel
     DEFAULT_DATA_PATH: relative path of data dir from toplevel
+    DEFAULT_MAP_PATH: relative path of id_map dir from toplevel
     
     DEFAULT_MYSQL_URL = location of MySQL db
     DEFAULT_MYSQL_PORT = port for MySQL db
@@ -22,13 +23,14 @@ Variables:
 
 DEFAULT_DOCKER_IMG = 'cblatti3/py3_mysql:0.1'
 DEFAULT_CURL_URL = 'mmaster02.cse.illinois.edu:4400'
-DEFAULT_LOCAL_BASE = '/mnt/users/blatti//apps/P1_source_check'
-DEFAULT_CLOUD_BASE = '/mnt/storage/blatti/apps/P1_source_check/'
+DEFAULT_LOCAL_BASE = '/workspace/apps/P1_source_check'
+DEFAULT_CLOUD_BASE = '/mnt/storage/post3/apps/P1_source_check/'
 
 DEFAULT_CODE_PATH = 'code'
 DEFAULT_DATA_PATH = 'raw_downloads'
+DEFAULT_MAP_PATH = 'id_map'
 
 DEFAULT_MYSQL_URL = 'knowcharles.cse.illinois.edu'
-DEFAULT_MYSQL_PORT = '3307'
+DEFAULT_MYSQL_PORT = '3308'
 DEFAULT_MYSQL_USER = 'root'
 DEFAULT_MYSQL_PASS = 'KnowEnG'
