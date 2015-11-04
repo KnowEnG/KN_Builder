02cacb70a6748820c962a7d6fa98c811afaa7468
Corey Post <cspost3@gmail.com>
2015-11-04 20:35:27 +0000

    added import_node and nullable n_alias in nodes

--- code/import_utilities.py
+++ code/import_utilities.py
@@ -16,7 +16,7 @@ import json
 import os
 
 
-def import_file(file_name, table, ld_cmd='', dup_cmd='', args=cf.config_args()):
+def import_file(file_name, table, ld_cmd='', dup_cmd='IGNORE', args=cf.config_args()):
     """Imports the provided  file into the KnowEnG MySQL database.
 
     Loads the data into a temporary table in MySQL. It then queries from the
@@ -36,14 +36,10 @@ def import_file(file_name, table, ld_cmd='', dup_cmd='', args=cf.config_args()):
     tmptable = os.path.splitext(os.path.basename(file_name))[0].replace('.', '-')
     db.create_temp_table(tmptable, 'LIKE ' + table)
     db.load_data(file_name, table, ld_cmd)
-    cmd = 'SELECT * FROM ' + tmptable + ' ON DUPLICATE KEY '
-    if dup_cmd:
-        cmd += dup_cmd
-    else:
-        cmd =+ 'IGNORE'
+    cmd = 'SELECT * FROM ' + tmptable + ' ON DUPLICATE KEY ' + dup_cmd
     db.insert(table, cmd)
 
-def import_edge(edgefile, args=cf.config_args()):
+def import_edgefile(edgefile, args=cf.config_args()):
     """Imports the provided edge file and any corresponding meta files into
     the KnowEnG MySQL database.
 
@@ -59,26 +55,23 @@ def import_edge(edgefile, args=cf.config_args()):
     Returns:
     """
     imports = ['node_meta', 'edge2line', 'edge', 'edge_meta']
-    uedge_cmd  = ('UPDATE weight = IF(weight > VALUES(weight), weight, '
-                    'VALUES(weight))')
     for table in imports:
         ld_cmd = ''
-        nm_cmd = ''
+        dup_cmd = ''
         if table == 'edge':
             filename = edgefile
-            dup_cmd = uedge_cmd
+            dup_cmd = ('UPDATE weight = IF(weight > VALUES(weight), weight, '
+                       'VALUES(weight))')
         else:
             filename = edgefile.replace('conv', table)
         if not os.path.isfile(filename):
             continue
         if table == 'node_meta':
-            import_node_meta(filename, args)
-        else:
-            import_file(filename, table, ld_cmd, nm_cmd, args)
+            ld_cmd = '(@dummy, column1, column2, column3)'
+        import_file(filename, table, ld_cmd, dup_cmd, args)
 
-def import_node_meta(nmfile, args=cf.config_args()):
-    """Imports the provided node_meta file and also imports all of the property
-    nodes that it contains into the KnowEnG MySQL database.
+def import_nodefile(nodefile, n_type_id, args=cf.config_args()):
+    """Imports the provided node file and into the KnowEnG MySQL database.
 
     Loads the data into a temporary table in MySQL. It then queries from the
     temporary table into the corresponding permanent table. If a duplication
@@ -86,21 +79,11 @@ def import_node_meta(nmfile, args=cf.config_args()):
     edge file, and ignores if it is metadata.
 
     Args:
-        nmfile (str): path to the file to be imported
+        nodefile (str): path to the file to be imported
         args: command line and default arguements
 
     Returns:
     """
-    nm_cmd = '(@dummy, column1, column2, column3)'
-    table = 'node_meta'
-    db = mu.get_database('KnowNet', args)
-    tmptable = os.path.splitext(os.path.basename(file_name))[0].replace('.', '-')
-    db.create_temp_table(tmptable, 'LIKE ' + table)
-    db.load_data(file_name, table, ld_cmd)
-    #load nodes
-    cmd = 'SELECT DISTINCT node_id, info_desc AS n_alias FROM ' + tmptable +
-          'WHERE info_type = "alt_alias" ON DUPLICATE KEY IGNORE'
-    db.insert('node', cmd)
-    #load node_meta
-    cmd = 'SELECT * FROM ' + tmptable + ' ON DUPLICATE KEY IGNORE'
-    db.insert(table, cmd)
+    ld_cmd = '(column1, column2) SET column3 = ' + str(n_type_id)
+    dup_cmd = ''
+    import_file(nodefile, 'node', ld_cmd, dup_cmd, args)
