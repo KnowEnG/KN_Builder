d0a9a48800bab3d142031256d616283e80fcb35a
Corey Post <cspost3@gmail.com>
2016-04-14 08:23:07 -0500

    added get aliases function to default source class

--- code/check_utilities.py
+++ code/check_utilities.py
@@ -80,6 +80,23 @@ class SrcClass(object):
         self.args = args
         self.chunk_size = 500000
 
+    def get_aliases(self):
+        """Helper function for producing the alias dictionary.
+
+        This returns a dictionary where alias names are keys and alias info
+        are the values. This helper function usse the species
+        specific information for the build of the Knowledge Network, which is
+        produced by ensembl.py during setup utilities and is located at
+        cf.DEFAULT_MAP_PATH/species/species.json, in order to fetch all matching
+        species specific aliases from the source.
+
+        Args:
+
+        Returns:
+            dict: A dictionary of species:(taxid, division) values
+        """
+        return dict()
+
     def get_source_version(self, alias):
         """Return the release version of the remote source:alias.
 
