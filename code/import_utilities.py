fcb89f9acd8c83bd97dc2129f9e20d7897826211
Corey Post <cspost3@gmail.com>
2016-06-08 17:50:26 +0000

    remove load into temporary table, use insert with dup commands

--- code/import_utilities.py
+++ code/import_utilities.py
@@ -39,44 +39,36 @@ def import_file(file_name, table, ld_cmd='', dup_cmd='', args=None):
         args=cf.config_args()
     db = mu.get_database('KnowNet', args)
     print('Inserting data from into ' + table)
-    db.load_data(file_name, table, ld_cmd)
-    return 1  ## remove this later (and potentially everything after)
-    if args is None:
-        args=cf.config_args()
     table_cmds = {'node_meta': 'node_meta.node_id = node_meta.node_id',
                 'node': 'node.node_id = node.node_id',
                 'raw_line' : 'raw_line.file_id = raw_line.file_id',
                 'edge2line': 'edge2line.edge_hash = edge2line.edge_hash',
                 'edge_meta': 'edge_meta.line_hash = edge_meta.line_hash',
                 'edge': ('edge.weight = IF(edge.weight > {0}.weight, edge.weight, '
-                    '{0}.weight)'),
-                'status': ('status.weight = IF(status.weight > {0}.weight, status.weight, '
                     '{0}.weight)')}
     if not dup_cmd and table in table_cmds:
         dup_cmd = table_cmds[table]
     db = mu.get_database('KnowNet', args)
-    tmptable = os.path.splitext(os.path.basename(file_name))[0].replace('.', '_')
-    tmptable = cf.pretty_name(tmptable, len(tmptable)).replace('-', '_')[:64]
-    print('Creating temporary table ' + tmptable)
-    db.create_temp_table(tmptable, 'LIKE ' + table)
-    print('Loading data into temporary table ' + tmptable)
-    db.load_data(file_name, tmptable, ld_cmd)
-    print('Inserting data from ' + tmptable + ' into ' + table)
-    cmd = 'SELECT * FROM ' + tmptable
-    db.start_transaction(level='READ UNCOMMITTED')
-    db.insert_ignore(table, cmd) #change later to duplicate
-    db.drop_table(tmptable)
-    db.close()
-    return 1  ## remove this later (and potentially everything after)
+    #tmptable = os.path.splitext(os.path.basename(file_name))[0].replace('.', '_')
+    #tmptable = cf.pretty_name(tmptable, len(tmptable)).replace('-', '_')[:64]
+    #print('Creating temporary table ' + tmptable)
+    #db.create_temp_table(tmptable, 'LIKE ' + table)
+    #print('Loading data into temporary table ' + tmptable)
+    #db.load_data(file_name, tmptable, ld_cmd)
+    #print('Inserting data from ' + tmptable + ' into ' + table)
+    #cmd = 'SELECT * FROM ' + tmptable
+    #db.start_transaction(level='READ UNCOMMITTED')
+    #db.insert_ignore(table, cmd) #change later to duplicate
+    #db.drop_table(tmptable)
+    #db.close()
+    #return 1  ## remove this later (and potentially everything after)
     if dup_cmd:
-        cmd += ' ON DUPLICATE KEY UPDATE ' + dup_cmd.format(tmptable)
-        db.start_transaction(level='READ COMMITTED')
-        db.insert(table, cmd)
+        ld_cmd += ' ON DUPLICATE KEY UPDATE ' + dup_cmd.format(table)
+        db.start_transaction(level='READ UNCOMMITTED')
+        db.insert(table, ld_cmd)
     else:
-        pass
-        db.start_transaction(level='READ COMMITTED')
-        db.replace(table, cmd)
-    db.drop_table(tmptable)
+        db.start_transaction(level='READ UNCOMMITTED')
+        db.replace(table, ld_cmd)
     db.close()
 
 def import_filemeta(version_dict, args=None):
