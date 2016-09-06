c7a93597bc57634ebed1593e87d58b7d02d6d64c
Aidan Epstein <aidan@jmad.org>
2016-09-06 10:42:48 -0500

    Some modifications to test_db.py

--- code/reports/test_db.py
+++ code/reports/test_db.py
@@ -5,8 +5,7 @@ import subprocess
 import glob
 import sqlite3
 
-p = subprocess.Popen(["mysql", "-hknownbs.dyndns.org", "-P3307", "-uKNviewer", "-pdbdev249", "-DKnowNet", "-eSELECT s.status_desc, s.table_hash FROM status s WHERE status = 'unmapped';"], stdout=subprocess.PIPE, universal_newlines=True)
-#p = subprocess.Popen(["cat", "tempdb.txt"], stdout=subprocess.PIPE, universal_newlines=True)
+p = subprocess.Popen(["mysql", "-hknownbs.dyndns.org", "-P3307", "-uKNviewer", "-pdbdev249", "-DKnowNet", "-eSELECT s.status_desc, s.n1_id, s.n2_id, s.table_hash FROM status s WHERE status = 'unmapped';"], stdout=subprocess.PIPE, universal_newlines=True)
 
 conn = sqlite3.connect(":memory:")
 conn.execute("""CREATE TABLE hashes (
@@ -17,19 +16,23 @@ conn.execute("""CREATE TABLE hashes (
         )""")
 conn.execute("""CREATE TABLE status (
         status_desc TEXT,
+        node1 TEXT,
+        node2 TEXT,
         table_hash TEXT,
         PRIMARY KEY (table_hash),
         FOREIGN KEY (table_hash) REFERENCES hashes(table_hash)
         )""")
 
-d = {}
 for fn in glob.glob('data_representative/*/*/chunks/*.table.*.txt'):
     csvr = csv.reader(open(fn, 'r'), delimiter='\t')
     conn.executemany("INSERT OR IGNORE INTO hashes (table_hash, node1, node2) VALUES (?, ?, ?)", ((line[-1], line[1], line[5]) for line in csvr))
 
 csvr = csv.reader(p.stdout, delimiter='\t')
 next(csvr)
-conn.executemany("INSERT INTO status (status_desc, table_hash) VALUES (?, ?)", (csvr))
+conn.executemany("INSERT INTO status (status_desc, node1, node2, table_hash) VALUES (?, ?, ?, ?)", (csvr))
 
-for line in conn.execute("SELECT s.table_hash, h.node1, h.node2, s.status_desc FROM status s, hashes h WHERE s.table_hash = h.table_hash"):
-    print('{} {},{} {}'.format(*line))
+for line in conn.execute("SELECT s.table_hash, h.node1, s.node1, h.node2, s.node2, s.status_desc FROM status s, hashes h WHERE s.table_hash = h.table_hash"):
+    if line[2].startswith('unmapped'):
+        print(line[1], line[2])
+    if line[4].startswith('unmapped'):
+        print(line[3], line[4])
