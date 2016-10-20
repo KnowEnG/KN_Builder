7140eb0ab3eba2f7b29175fa5c9b696e2c91d588
Aidan Epstein <aidan@jmad.org>
2016-10-20 15:51:20 -0500

    FIx some errors, make some improvements.

--- code/export_utilities.py
+++ code/export_utilities.py
@@ -6,10 +6,10 @@ import boto3, botocore
 import os
 
 def get_gg(db, et, taxon = 9606):
-    return db.run("SELECT s.edge_hash, s.n1_id, s.n2_id, s.weight, s.et_name, rl.file_id, rl.line_num FROM status s JOIN node_species n1 ON s.n1_id = n1.node_id JOIN node_species n2 ON s.n2_id = n2.node_id JOIN raw_line rl ON s.line_hash = rl.line_hash WHERE s.et_name = '{}' AND n1.taxon = {} AND n2.taxon = {} AND s.status = 'production';".format(et, taxon, taxon))
+    return db.run("SELECT s.n1_id, s.n2_id, s.weight, s.et_name, rl.file_id, rl.line_num FROM status s JOIN node_species n1 ON s.n1_id = n1.node_id JOIN node_species n2 ON s.n2_id = n2.node_id JOIN raw_line rl ON s.line_hash = rl.line_hash WHERE s.et_name = '{}' AND n1.taxon = {} AND n2.taxon = {} AND s.status = 'production';".format(et, taxon, taxon))
 
 def get_pg(db, et, taxon = 9606):
-    return db.run("SELECT s.edge_hash, s.n1_id, s.n2_id, s.weight, s.et_name, rl.file_id, rl.line_num FROM status s JOIN node_species n2 ON s.n2_id = n2.node_id JOIN raw_line rl ON s.line_hash = rl.line_hash WHERE s.et_name = '{}' AND n2.taxon = {} AND s.status = 'production';".format(et, taxon))
+    return db.run("SELECT s.n1_id, s.n2_id, s.weight, s.et_name, rl.file_id, rl.line_num FROM status s JOIN node_species n2 ON s.n2_id = n2.node_id JOIN raw_line rl ON s.line_hash = rl.line_hash WHERE s.et_name = '{}' AND n2.taxon = {} AND s.status = 'production';".format(et, taxon))
 
 
 def figure_out_class(db, et):
@@ -21,15 +21,15 @@ def norm_edges(edges):
 def get_nodes(edges):
     ret = set()
     for i in edges:
+        ret.add(i[0])
         ret.add(i[1])
-        ret.add(i[2])
     return ret
 
 def convert_genes(db, nodes):
     return db.run("SELECT node_id, n_alias FROM node WHERE node_id IN ('{}')".format("','".join(nodes)))
 
-def get_sources(db, edges):
-    return db.run("SELECT DISTINCT(file_id) FROM raw_line rl JOIN edge2line e2l ON rl.line_hash = e2l.line_hash WHERE e2l.edge_hash IN ('{}')".format("','".join(edge[0] for edge in edges)))
+def get_sources(edges):
+    return list(set(edge[4] for edge in edges))
 
 def get_metadata(db, sources):
     return {"sources": sources}
@@ -51,7 +51,7 @@ def main():
     edges_fn = 'network.edges'
     nodes_fn = 'nodes.meta'
     meta_fn = 'sources.meta'
-    bucket_dir = os.path.join(cls, args.edge_type, args.species)
+    bucket_dir = os.path.join(cls, args.species, args.edge_type)
     sync_dir = os.path.join(args.working_dir, args.bucket_name, bucket_dir)
     sync_edges = os.path.join(sync_dir, edges_fn)
     sync_nodes = os.path.join(sync_dir, nodes_fn)
@@ -64,11 +64,13 @@ def main():
     s3edges = s3.Object(args.bucket_name, bucket_edges)
     s3nodes = s3.Object(args.bucket_name, bucket_nodes)
     s3meta = s3.Object(args.bucket_name, bucket_meta)
-    try:
-        if s3edges.content_length > 0 and s3nodes.content_length > 0 and s3meta.content_length > 0:
-            return
-    except botocore.exceptions.ClientError as e: #Maybe check to make sure the error is not found?
-        pass
+
+    if not args.force_fetch:
+        try:
+            if s3edges.content_length > 0 and s3nodes.content_length > 0 and s3meta.content_length > 0:
+                return
+        except botocore.exceptions.ClientError as e: #Maybe check to make sure the error is not found?
+            pass
 
     get = get_gg if cls == 'Gene' else get_pg
     res = get(db, args.edge_type)
@@ -77,7 +79,7 @@ def main():
     nodes = get_nodes(res)
     nodes_desc = convert_genes(db, nodes)
 
-    sources = get_sources(db, res)
+    sources = get_sources(res)
     metadata = get_metadata(db, sources)
     db.close()
 
