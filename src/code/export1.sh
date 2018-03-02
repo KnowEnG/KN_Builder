cd0793c1143a3ec02de700b7033d6146f2bb6e79
cblatti3 <blatti@illinois.edu>
2018-03-02 11:41:07 +0000

    change exports for new build_status

--- src/code/export1.sh
+++ src/code/export1.sh
@@ -2,6 +2,7 @@
 
 set -u
 
+KNP_EXPORT_DIR=$KNP_WORKING_DIR/$KNP_EXPORT_PATH
 mkdir -p $KNP_EXPORT_DIR
 cp $KNP_CODE_DIR/mysql/edge_type.txt $KNP_EXPORT_DIR
 
@@ -10,7 +11,7 @@ cp $KNP_WORKING_DIR/$KNP_DATA_PATH/id_map/species/species.txt $KNP_EXPORT_DIR/sp
 for TAXON in `cut -f1 $KNP_EXPORT_DIR/species.txt `; do
     echo $TAXON;
     mkdir -p $KNP_EXPORT_DIR/Species/$TAXON;
-    mysql -h$KNP_MYSQL_HOST -u$KNP_MYSQL_USER -p$KNP_MYSQL_PASS -P$KNP_MYSQL_PORT -D$KNP_MYSQL_DB -e "\
+    mysql -h$KNP_MYSQL_HOST -u$KNP_MYSQL_USER -p$KNP_MYSQL_PASS -P$KNP_MYSQL_PORT -DKnowNet -e "\
         SELECT ns.node_id \
         FROM node_species ns \
         WHERE ns.taxon = $TAXON \
@@ -36,13 +37,7 @@ awk -v x=125000 '$4 >= x' $KNP_EXPORT_DIR/db_contents.txt \
     | grep "^Gene" >> $KNP_EXPORT_DIR/directories.txt
 awk -v x=4000 '$4 >= x' $KNP_EXPORT_DIR/db_contents.txt \
     | grep "^Property" >> $KNP_EXPORT_DIR/directories.txt
-python3 $KNP_CODE_DIR/workflow_utilities.py EXPORT \
-    -myh $KNP_MYSQL_HOST -myp $KNP_MYSQL_PORT \
-    -myps $KNP_MYSQL_PASS -myu $KNP_MYSQL_USER \
-    -rh $KNP_REDIS_HOST -rp $KNP_REDIS_PORT \
-    -wd $KNP_WORKING_DIR -dp $KNP_DATA_PATH -lp $KNP_LOGS_PATH \
-    -c $KNP_CHRONOS_URL -b $KNP_EXPORT_DIR \
-    -sd $KNP_STORAGE_DIR -es $KNP_ENS_SPECIES \
+python3 $KNP_CODE_DIR/workflow_utilities.py EXPORT $KNP_CONFIG_OPTS \
     -p "$(tail -n+2 $KNP_EXPORT_DIR/directories.txt \
         | cut -f2,3 \
         | sed -e 's/\t/::/g' \
--- src/code/export2.sh
+++ src/code/export2.sh
@@ -9,6 +9,7 @@ for CLASS1 in Property; do
         CLASS=$(echo $line | cut -f1 -d/)
         TAXON=$(echo $line | cut -f2 -d/)
         ETYPE=$(echo $line | cut -f3 -d/)
+        mkdir -p $KNP_EXPORT_DIR/$CLASS/$TAXON/$ETYPE/
         grep Property $KNP_EXPORT_DIR/$CLASS/$TAXON/$ETYPE/$TAXON.$ETYPE.node_map > $KNP_EXPORT_DIR/$CLASS/$TAXON/$ETYPE/$TAXON.$ETYPE.pnode_map
     done
 done;
